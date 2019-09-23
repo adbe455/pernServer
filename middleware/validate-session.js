f3e5175f647c5e30d96d513c011a2c1f1daee06a
Aaron Davis <adbe455@gmail.com>
2019-09-23 13:31:17 -0400

    validate session

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,25 +1,56 @@
-const jwt = require('jsonwebtoken');
-const User = require('../db').import('../models/user');
-const validateSession = (req, res, next) => {
-   // console.log(req);
-   const token = req.headers.authorization;
-   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-       if(!err && decoded){
-           User.findOne({
-           where: {
-               id: decoded.id
-           }
-       }, console.log(decoded))
-       .then(user => {
-           if(!user) throw 'err'
-           req.user = user
-           return next();
-       })
-       .catch(err => next(err))
-       } else {
-           req.error = err
-           return res.status(500).send('Not Authorized')
-       }
-   })
+var jwt = require('jsonwebtoken');
+var sequelize = require('../db');
+var User = sequelize.import('../models/user');
+
+module.exports = function(req, res, next) {
+     if (req.method == 'OPTIONS') {
+         next()
+     } else {
+        var sessionToken = req.headers.authorization;
+        console.log(sessionToken)
+        if (!sessionToken) return res.status(403).send({ auth: false, message: 'No token provided.' });
+        else {
+            jwt.verify(sessionToken, process.env.JWT_SECRET, (err, decoded) => {
+                if(decoded){
+                    User.findOne({where: { id: decoded.id}}).then(user => {
+                        req.user = user;
+                        next();
+                    },
+                    function(){
+                        res.status(401).send({error: 'Not authorized'});
+                    });
+                } else {
+                    res.status(400).send({error: 'Not authorized'});
+                }
+            });
+        }
+    }
 }
-module.exports = validateSession;
+
+
+
+// const jwt = require('jsonwebtoken');
+// const User = require('../db').import('../models/user');
+// const validateSession = (req, res, next) => {
+//    // console.log(req);
+//    const token = req.headers.authorization;
+//    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//        if(!err && decoded){
+//            User.findOne({
+//            where: {
+//                id: decoded.id
+//            }
+//        }, console.log(decoded))
+//        .then(user => {
+//            if(!user) throw 'err'
+//            req.user = user
+//            return next();
+//        })
+//        .catch(err => next(err))
+//        } else {
+//            req.error = err
+//            return res.status(500).send('Not Authorized')
+//        }
+//    })
+// }
+// module.exports = validateSession;
